472bba7154214cdd64201dc8495135b76284bc69
jlord <jessica.lord@github.com>
2013-12-18 22:31:48 -0800

    moar challenges

--- problems/get_git/setup.js
+++ problems/get_git/setup.js
@@ -1,1 +1,25 @@
-setup.js
+#!/usr/bin/env node
+
+var spawn = require('child_process').spawn
+var concat = require('concat-stream')
+
+var username = spawn('git', ['config', 'user.name'])
+var email =  spawn('git', ['config', 'user.email'])
+
+username.stdout.pipe(concat(onUsername))
+email.stdout.pipe(concat(onEmail))
+
+var pending = 2
+
+function onUsername(output) {
+  var userOutput = output.toString().trim()
+  if (userOutput == "") console.error("error")
+  else console.log("Awesome", userOutput, "!" )
+}
+
+function onEmail(output) {
+  var emailOutput = output.toString().trim()
+  if (emailOutput == "") console.error("error")
+  else console.log("cool")
+}
+
