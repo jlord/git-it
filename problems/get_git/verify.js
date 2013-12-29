8f7738d915e35db4ab25f3a90e1b34f626b46a41
jlord <jessica.lord@github.com>
2013-12-28 17:50:24 -0800

    repo verify

--- problems/get_git/verify.js
+++ problems/get_git/verify.js
@@ -7,19 +7,16 @@ var username = spawn('git', ['config', 'user.name'])
 var email =  spawn('git', ['config', 'user.email'])
 
 username.stdout.pipe(concat(onUsername))
-// email.stdout.pipe(concat(onEmail))
-
-// var pending = 2
 
 function onUsername(output) {
   var userOutput = output.toString().trim()
   if (userOutput == "") console.error("error")
-  else console.log(userOutput); email.stdout.pipe(concat(onEmail))
+  else email.stdout.pipe(concat(onEmail))
 }
 
 function onEmail(output) {
   var emailOutput = output.toString().trim()
   if (emailOutput == "") console.error("error")
-  else console.log(emailOutput)
+  else console.log(true)
 }
 
---
+++ problems/repository/verify.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+// should have them type in the path of their
+// repo OR have them create a new folder from 
+// the start.
+
+var spawn = require('child_process').spawn
+var concat = require('concat-stream')
+
+var status = spawn('git', ['status'])
+
+status.stdout.pipe(concat(onStatus))
+
+function onStatus(output) {
+  var status = output.toString().trim()
+  if (status != "fatal: Not a git repository (or any of the parent directories): .git")
+  console.log(true)
+}
