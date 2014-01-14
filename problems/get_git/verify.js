f601394ce1b755291d63ea61ead8a820e1caf70f
jlord <jessica.lord@github.com>
2014-01-13 21:50:31 -0800

    move git config to remote control

--- problems/get_git/verify.js
+++ problems/get_git/verify.js
@@ -3,20 +3,35 @@
 var spawn = require('child_process').spawn
 var concat = require('concat-stream')
 
-var username = spawn('git', ['config', 'user.name'])
-var email =  spawn('git', ['config', 'user.email'])
+// Change to just check that Git is installed
 
-username.stdout.pipe(concat(onUsername))
+//var username = spawn('git', ['config', 'user.name'])
+//var email =  spawn('git', ['config', 'user.email'])
+//
+//username.stdout.pipe(concat(onUsername))
+//
+//function onUsername(output) {
+//  var userOutput = output.toString().trim()
+//  if (userOutput == "") console.error("error")
+//  else email.stdout.pipe(concat(onEmail))
+//}
+//
+//function onEmail(output) {
+//  var emailOutput = output.toString().trim()
+//  if (emailOutput == "") console.error("error")
+//  else console.log(true)
+//}
 
-function onUsername(output) {
-  var userOutput = output.toString().trim()
-  if (userOutput == "") console.error("error")
-  else email.stdout.pipe(concat(onEmail))
+var git = spawn('git', ['--version'])
+
+git.stdout.pipe(concat(onGit))
+
+function onGit(output) {
+  var gitOutput = output.toString().trim()
+  if (gitOutput.match("git version")) {
+    console.log(true)    
+  }
 }
 
-function onEmail(output) {
-  var emailOutput = output.toString().trim()
-  if (emailOutput == "") console.error("error")
-  else console.log(true)
-}
+
 
