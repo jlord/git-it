0a0f2d7454ea17be9e0f1c4db6cd6ee5f2925408
jlord <jessica.lord@github.com>
2014-01-14 16:46:55 -0800

    re arrange lesson content

--- problems/pull/verify.js
+++ problems/pull/verify.js
@@ -2,46 +2,16 @@
 
 var spawn = require('child_process').spawn
 var concat = require('concat-stream')
-
 var pull = spawn('git', ['fetch', '--dry-run'])
 
+// do a fetch dry run to see if there is anything 
+// to pull; if there is they haven't pulled yet
 
-pull.stdout.pipe(concat(onRemote))
+pull.stdout.pipe(concat(onPull))
 
-function onRemote(output) {
+function onPull(output) {
   var status = output.toString().trim()
   if (!status)
   console.log(true)
   else console.log("There are changes to pull in")
 }
-
-// check via the github api? would need username
-// and repo name (repo name could be assumed/gotten
-// via the name of the current directory locally)
-
-// git show-branch origin/master
-
-// var remote = spawn('git', ['show-branch', 'origin/master'])
-
-// If commits but no remote pushes
-//
-// jlord:test jessicalord$ git show-branch origin/master
-// fatal: bad sha1 reference origin/master
-
-// If empty git repo, no commits
-//
-// jlord:test jessicalord$ git show-branch master
-// fatal: bad sha1 reference master
-//
-// git remote returns nothing
-
-// If no pushes
-//
-// jlord:test jessicalord$ git reflog show origin/master
-// fatal: ambiguous argument 'origin/master': unknown revision or path not in the working tree.
-// Use '--' to separate paths from revisions, like this:
-// 'git <command> [<revision>...] -- [<file>...]'
-
-// jlord:git-it jessicalord$ git reflog show origin/master
-// c63d2bc refs/remotes/origin/master@{0}: update by push
-// c11f47a refs/remotes/origin/master@{1}: update by push
