732125f2d8bd0cb2386247e25444abdb67f378c1
Jessica Lord <jessica.lord@github.com>
2014-01-29 19:12:12 -0800

    missing bracket

--- problems/commit_to_it/verify.js
+++ problems/commit_to_it/verify.js
@@ -7,9 +7,10 @@ var exec = require('child_process').exec
 exec('git status', function(err, stdout, stdrr) {
   var show = stdout.trim()
   
-  if (show.match("nothing to commit"))
+  if (show.match("nothing to commit")) {
     console.log("Changes have been committed!")
-  else if (show.match("Changes not staged for commit") {
+  }
+  else if (show.match("Changes not staged for commit")){
     console.log("Seems there are changes\nto commit still.")
-  } else ("Hmm, can't find\ncommitted changes.")
+  } else console.log("Hmm, can't find\ncommitted changes.")
 })
