18aeda4d9ec315b03ec696a951dd9d530e81f121
Jessica Lord <jessica.lord@github.com>
2014-07-05 20:01:11 -0700

    working on builder

--- guide/buildpages.js
+++ guide/buildpages.js
@@ -1,2 +1,31 @@
-var hbs = require('handlebars')
+var Handlebars = require('handlebars')
 var fs = require('fs')
+var glob = require('glob')
+
+var layout = fs.readFileSync(__dirname + '/layout.hbs').toString()
+
+// I can probably use glob better to avoid
+// finding the right files within the files
+glob("**/*.html", function (err, files) {
+  // console.log(files)
+  if (err) return console.log(err)
+  var matches = files.map(function(file) {
+    if (file.match('guide/raw-content/')) {
+      return file
+    }
+  })
+  // console.log(matches)
+  buildPage(matches)
+})
+
+function buildPage(files) {
+  files.forEach(function(file) {
+    if (!file) return
+    var content = {header: "hi", footer: "bye", body: fs.readFileSync(file).toString()}
+    var template = Handlebars.compile(layout)
+    var final = template(content)
+    console.log(final)
+  })
+}
+
+
