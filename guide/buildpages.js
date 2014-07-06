4b2de2b84af67655caf9a1167080b4a7fa1c11ee
Jessica Lord <jessica.lord@github.com>
2014-07-05 21:01:03 -0700

    build pages

--- guide/buildpages.js
+++ guide/buildpages.js
@@ -7,25 +7,36 @@ var layout = fs.readFileSync(__dirname + '/layout.hbs').toString()
 // I can probably use glob better to avoid
 // finding the right files within the files
 glob("**/*.html", function (err, files) {
-  // console.log(files)
   if (err) return console.log(err)
   var matches = files.map(function(file) {
     if (file.match('guide/raw-content/')) {
       return file
     }
   })
-  // console.log(matches)
   buildPage(matches)
 })
 
 function buildPage(files) {
+  console.log()
   files.forEach(function(file) {
+    // shouldn't have to do this if my
+    // mapping were correct
     if (!file) return
-    var content = {header: "hi", footer: "bye", body: fs.readFileSync(file).toString()}
+    var content = { 
+      header: "hi", 
+      footer: "bye", 
+      body: fs.readFileSync(file).toString()
+    }
+    var shortname = makeShortname(file)
     var template = Handlebars.compile(layout)
     var final = template(content)
-    console.log(final)
+    fs.writeFileSync(__dirname + '/challenges/' + shortname + '.html', final)
   })
 }
+
+function makeShortname(filename) {
+  return filename.split('/').pop().replace('html','')
+}
+
 
 
