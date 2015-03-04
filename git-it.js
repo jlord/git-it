bb052d029100633133d3fcc3cbf15ecc5d9cc19d
hygkui <fevernova.life@gmail.com>
2015-03-04 20:26:21 +0800

    fix the issue #148. for different env.LANG setting, the verify result will always be in English.

--- git-it.js
+++ git-it.js
@@ -3,6 +3,8 @@
 const Workshopper = require('workshopper-jlord'),
       path = require('path')
 
+process.env.LANG = 'en_US.UTF-8'
+
 Workshopper({
   name: 'git-it',
   title: 'GIT + GITHUB : VERSION CONTROL + SOCIAL CODING',
